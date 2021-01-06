aaaf4d140f29663cc40ba4ec1748225a6533d275
michasacuer <michasacuer3@gmail.com>
2021-01-06 19:59:02 +0100

    dd

---
+++ Benchmarking/ListVsHashSet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarking
+{
+    public class Class1
+    {
+        public ICollection<string> HashSet { get; set; } = new HashSet<string>(new[] {"a", "b", "c", "d", "e"});
+    }
+    
+    public class Class2
+    {
+        public ICollection<string> List { get; set; } = new List<string>(new[] {"a", "b", "c", "d", "e"});
+    }
+    
+    [MemoryDiagnoser]
+    public class ListVsHashSet
+    {
+        [Benchmark]
+        public void HashSet()
+        {
+            var list1 = new List<Class1>();
+            
+            for (int i = 0; i < 100000; i++)
+            {
+                list1.Add(new Class1());
+            }
+        }
+        
+        [Benchmark]
+        public void List()
+        {
+            var list2 = new List<Class2>();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                list2.Add(new Class2());
+            }
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<AddRangeVsConcat>();
+            var summary = BenchmarkRunner.Run<ListVsHashSet>();
         }
     }
 }
